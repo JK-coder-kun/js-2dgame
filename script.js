814cfbe84e0a36a5245d03c2860837baea253aa0
JK-coder-kun <JK-coder-kun@github.com>
2023-05-13 16:16:53 +0630

    NPC movement enemy2

--- script.js
+++ script.js
@@ -12,20 +12,25 @@ let enemiesArray = [];
 class Enemy{
     constructor(){
         this.image = new Image();
-        this.image.src = "enemy1.png"
-        // this.speed = Math.random()*4 - 2;
-        this.spriteWidth = 293;
-        this.spriteHeight = 155;
+        this.image.src = "enemy2.png"
+        this.speed = Math.random()*4 + 1;
+        this.spriteWidth = 266;
+        this.spriteHeight = 188;
         this.width = this.spriteWidth/2.5;
         this.height = this.spriteHeight/2.5;
         this.x = Math.random()*(CANVAS_WIDTH-this.width);
         this.y = Math.random()*(CANVAS_HEIGHT-this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random()*3 +1);
+        this.angle = Math.random() * 2;
+        this.angleSpeed = Math.random() * 0.2;
+        this.curve = Math.random() * 7;
     }
     update(){
-        this.x += Math.random() * 5 -2.5;
-        this.y += Math.random() * 5 -2.5;
+        if(this.x + this.width < 0 )this.x = CANVAS_WIDTH;
+        this.x -= this.speed;
+        this.y += this.curve * Math.sin(this.angle);
+        this.angle += this.angleSpeed;
         //animate sprite
         if(gameFrame % this.flapSpeed === 0 ){
             this.frame > 4 ? this.frame = 0 : this.frame++;
