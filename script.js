9074e65cca5bff0096e8f63275188b733a7a90fc
JK-coder-kun <JK-coder-kun@github.com>
2023-05-13 16:01:40 +0630

    npc movements enemy1

--- script.js
+++ script.js
@@ -1,70 +1,53 @@
-const canvas=document.getElementById("canvas1");
-const ctx=canvas.getContext("2d");
-const CANVAS_WIDTH = canvas.width = 800;
-const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 5;
-// let gameFrames = 0;
+/**@type{HTMLCanvasElement} */
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+CANVAS_WIDTH = canvas.width = 500;
+CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 10;
+let gameFrame = 0;
+let enemiesArray = [];
 
-const backgroundLayer1 = new Image();
-backgroundLayer1.src="layer-1.png";
-const backgroundLayer2 = new Image();
-backgroundLayer2.src="layer-2.png";
-const backgroundLayer3 = new Image();
-backgroundLayer3.src="layer-3.png";
-const backgroundLayer4 = new Image();
-backgroundLayer4.src="layer-4.png";
-const backgroundLayer5 = new Image();
-backgroundLayer5.src="layer-5.png";
 
-window.addEventListener('load',function(){
-    const slider = document.getElementById("slider");
-    slider.value = gameSpeed;
-    const showGameSpeed = document.getElementById("showGameSpeed");
-    showGameSpeed.innerHTML = gameSpeed;
-    slider.addEventListener("change",(e)=>{
-        gameSpeed = e.target.value;
-        showGameSpeed.innerHTML = gameSpeed;
+
+class Enemy{
+    constructor(){
+        this.image = new Image();
+        this.image.src = "enemy1.png"
+        // this.speed = Math.random()*4 - 2;
+        this.spriteWidth = 293;
+        this.spriteHeight = 155;
+        this.width = this.spriteWidth/2.5;
+        this.height = this.spriteHeight/2.5;
+        this.x = Math.random()*(CANVAS_WIDTH-this.width);
+        this.y = Math.random()*(CANVAS_HEIGHT-this.height);
+        this.frame = 0;
+        this.flapSpeed = Math.floor(Math.random()*3 +1);
+    }
+    update(){
+        this.x += Math.random() * 5 -2.5;
+        this.y += Math.random() * 5 -2.5;
+        //animate sprite
+        if(gameFrame % this.flapSpeed === 0 ){
+            this.frame > 4 ? this.frame = 0 : this.frame++;
+        }
+    }
+    draw(){
+        ctx.drawImage(this.image,this.frame * this.spriteWidth ,0 ,this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    }
+};
+
+for(let i = 0; i < numberOfEnemies; i++){
+    enemiesArray.push(new Enemy());
+}
+
+function animate(){
+    ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    enemiesArray.forEach(enemy=>{
+        enemy.update();
+        enemy.draw();
     })
+    gameFrame++;
+    requestAnimationFrame(animate);
+}
 
-    class layer{
-        constructor(image, speedModifier){
-            this.x = 0;
-            this.y = 0;
-            this.width = 2400;
-            this.height = 700;
-            this.image = image;
-            this.speedModifier = speedModifier;
-            this.speed = gameSpeed * this.speedModifier;
-        }
-        update(){
-            this.speed = gameSpeed * this.speedModifier;
-            if(this.x <= -this.width) this.x = 0;
-            this.x = this.x - this.speed;
-            // this.x = (gameFrames*this.speed) % this.width;
-        }
-        draw(){
-            ctx.drawImage(this.image, this.x,this.y,this.width,this.height);
-            ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
-        }
-    }
-
-    const layer1 = new layer(backgroundLayer1, 0.5);
-    const layer2 = new layer(backgroundLayer2, 0.5);
-    const layer3 = new layer(backgroundLayer3, 0.5);
-    const layer4 = new layer(backgroundLayer4, 0.5);
-    const layer5 = new layer(backgroundLayer5, 1);
-
-    const gameObjects = [layer1,layer2,layer3,layer4,layer5];
-
-    function animate(){
-        ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-        gameObjects.forEach(object=>{
-            object.update();
-            object.draw();
-        })
-        // gameFrames--;
-        requestAnimationFrame(animate);
-    }
-
-    animate();
-});
+animate();
